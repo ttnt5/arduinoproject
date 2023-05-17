b66535fd974f7aea48b2bfdb967ef861b13cce04
ttnt5 <61551694+ttnt5@users.noreply.github.com>
2023-05-17 10:43:30 +0900

    add

---
+++ 2023.05.17/project/project.ino
@@ -0,0 +1,78 @@
+#include <SoftwareSerial.h>
+#include <DHT.h>
+#define Buse 2
+#define fanpin 3
+#define ledpin1 4
+#define ledpin2 5
+#define RxD 6
+#define TxD 7
+#define DHTport A0
+#define lightport A1
+#define Gasport A2
+
+DHT myDHT(DHTport,DHT11);
+SoftwareSerial bt(RxD,TxD);
+
+void setup()
+{
+  pinMode(Buse, OUTPUT);
+  pinMode(Gasport, OUTPUT);
+  pinMode(fanpin, OUTPUT);
+  pinMode(ledpin1, OUTPUT);
+  Serial.begin(9600);
+  bt.begin(38400);
+  myDHT.begin();
+  bt.print("\r\n+STWMOD=0\r\n");  
+  delay(2000);
+  bt.print("\r\n+INQ=1\r\n");
+  delay(2000); 
+  bt.flush();
+}
+
+void loop()
+{
+  float tempa = myDHT.readTemperature();
+  float Humia = myDHT.readHumidity();
+  float gas = analogRead(Gasport);
+  int light = analogRead(lightport);
+  
+  bt.print("Value" + String(tempa));
+  bt.print(String(Humia));
+
+  if (gas <50){
+    bt.print("정상");
+  }
+  if (gas >50){
+    bt.print("위험");
+    digitalWrite(Buse, HIGH);
+    digitalWrite(ledpin1, LOW);
+    delay(100);
+    digitalWrite(Buse, LOW);
+    digitalWrite(ledpin1, HIGH);
+  }
+
+   if (tempa > 25 ||Humia > 41 || gas > 50)
+  {
+    digitalWrite(fanpin, LOW);
+    bt.print("0");
+  }
+  else{
+    digitalWrite(fanpin, HIGH);
+    bt.print("1");
+  }
+
+  if (light > 10){
+    digitalWrite(lightport, HIGH);
+    bt.print("1");
+  }
+  else{
+    digitalWrite(lightport, LOW);
+    bt.print("0");
+  }
+  
+  //Serial.println("온도" + String(tempa));
+  //Serial.println("습도" + String(Humia));
+  //Serial.println("가스" + String(gas));
+  //Serial.println("조도값" + String(light));
+  delay(1000);
+}
